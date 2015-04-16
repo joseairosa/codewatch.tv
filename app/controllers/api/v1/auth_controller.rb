efe8fc2fa46044cabebf49ac44dbe45453dbf291
José P. Airosa <me@joseairosa.com>
2015-04-16 16:07:26 +0100

    Removed pry

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::AuthController < Api::V1::ApiController
   def stream
     valid = User.valid_stream_key?(params[:name], params[:stream_key])
-    require 'pry'; binding.pry
     respond_to do |format|
       if valid
         format.json { render json: {auth:'ok'}, status: 200 }
