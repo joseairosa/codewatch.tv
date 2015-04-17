1292433f58fb0a63a9697f9bedef63f5d594eb63
José P. Airosa <me@joseairosa.com>
2015-04-17 15:55:58 +0100

    Statistics and recordings

---
+++ app/controllers/api/v1/video_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::VideoController < Api::V1::ApiController
+  def stats
+    user = User.where(username: params[:name]).first
+    if user
+      case params[:event]
+        when 'play'
+          user.channel.new_viewer
+        when 'play_done'
+          user.channel.viewer_left
+        else
+          # do nothing
+      end
+    end
+    respond_to do |format|
+      format.json { render json: {auth:'ok'}, status: 200 }
+    end
+  end
+
+  def new_recording
+    user = User.where(username: params[:name]).first
+    if user
+      Recording.create!(user: user, name: params[:path])
+    end
+    respond_to do |format|
+      format.json { render json: {auth:'ok'}, status: 200 }
+    end
+  end
+end
