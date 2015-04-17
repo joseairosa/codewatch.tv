9b0c53f25745575037339a5067af6b57a43135fc
Jorja Hung <jorjahung@gmail.com>
2015-04-17 15:18:11 +0100

    Integrated with Facebook omniauth [TODO: allow users to change username]

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,18 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    if request.env["omniauth.auth"].info.email.blank?
+      redirect_to "/users/auth/facebook?auth_type=rerequest&scope=email"
+    end
+
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    require 'pry'; binding.pry
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      flash[:notice] << " Please don't forget to change your username." if @user.email == @user.username
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -70,6 +70,25 @@ class User
     self.where(username: username, stream_key: stream_key).count == 1
   end
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.username = auth.info.email
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.save
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
   private
 
   def create_channel
