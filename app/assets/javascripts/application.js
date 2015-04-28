83d0837cc0d2eb6abca617306ec5d0e000d77065
José P. Airosa <me@joseairosa.com>
2015-04-28 15:59:06 +0100

    Added alerts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,10 @@
 //= require_tree .
 
 jwplayer.key="RdBgRDffK8CxoR+GCaOb0iinIDch8TJmzRxvoA==";
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +  s4() + '-' + s4() + s4() + s4();
+}
