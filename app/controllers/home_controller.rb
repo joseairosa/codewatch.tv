46073ef5d3657e02ce48d9acfe1d091ba68e65e7
José P. Airosa <me@joseairosa.com>
2015-05-05 14:53:37 +0100

    Looking a bit better

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,25 @@
 class HomeController < ApplicationController
   include ChannelHelper
 
+  TOP_LANGUAGES = %w(Java C C++ C# Python JavaScript PHP Ruby Objective-C Go)
+
   helper_method :featured_channels
   helper_method :featured_streamers
+  helper_method :top_categories
+  helper_method :top_languages
 
   def index
   end
 
   private
+
+  def top_languages
+    TOP_LANGUAGES
+  end
+
+  def top_categories
+    @top_categories ||= Category.in(name: TOP_LANGUAGES).inject({}) { |res, category| res[category.name] = category; res }
+  end
 
   def featured_channels
     first_channel = Channel.first
