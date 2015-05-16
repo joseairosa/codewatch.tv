3c86bfac1f45c7fb8bbb2f7e047634821bc617d6
José P. Airosa <me@joseairosa.com>
2015-05-16 22:59:25 +0100

    I'm retarded

--- app/controllers/api/v1/stream_controller.rb
+++ app/controllers/api/v1/stream_controller.rb
@@ -13,11 +13,11 @@ class Api::V1::StreamController < Api::V1::ApiController
           if params[:app] == 'vod'
             recording = Recording.find(File.basename(params[:pageurl]))
             if recording
-              ChannelService.instance.update_recording_viewers(recording, recording.current_viewers, adjustment: 1)
+              ChannelService.instance.update_recording_viewers(recording, recording.current_viewers, 1)
             end
           elsif params[:app] == 'watch'
             StatisticService.instance.watching_quality(quality)
-            ChannelService.instance.update_live_viewers(user.channel, adjustment: 1)
+            ChannelService.instance.update_live_viewers(user.channel, 1)
           end
 
           {json: {auth: 'ok'}, status: 200}
@@ -25,10 +25,10 @@ class Api::V1::StreamController < Api::V1::ApiController
           if params[:app] == 'vod'
             recording = Recording.find(File.basename(params[:pageurl]))
             if recording
-              ChannelService.instance.update_recording_viewers(recording, recording.current_viewers, adjustment: -1)
+              ChannelService.instance.update_recording_viewers(recording, recording.current_viewers, -1)
             end
           elsif params[:app] == 'watch'
-            ChannelService.instance.update_live_viewers(user.channel, adjustment: -1)
+            ChannelService.instance.update_live_viewers(user.channel, -1)
           end
 
           {json: {auth: 'ok'}, status: 200}
