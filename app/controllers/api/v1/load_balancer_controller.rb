49ef396e2048e61ac5293426a89363d63adbeaa9
José P. Airosa <me@joseairosa.com>
2015-06-13 19:58:31 +0100

    Attempting to use local IPs instead

--- app/controllers/api/v1/load_balancer_controller.rb
+++ app/controllers/api/v1/load_balancer_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::LoadBalancerController < Api::V1::ApiController
   def stream
     user = User.where(username: params[:name], stream_key: params[:stream_key]).first
     response = if user
-                 {json: {response: 'ok'}, status: 302, location: "rtmp://#{STREAMERS_CONFIG[user.streamer_id]['ip']}/#{params[:app]}/#{params[:name]}?stream_key=#{params[:stream_key]}"}
+                 {json: {response: 'ok'}, status: 302, location: "rtmp://#{STREAMERS_CONFIG[user.streamer_id]['private-ip']}/#{params[:app]}/#{params[:name]}?stream_key=#{params[:stream_key]}"}
                else
                  {json: {response:'user_streamer_id_not_found'}, status: 404}
                end
