b29c3cb145cf6b8426a773b8975be1776e50515a
José P. Airosa <me@joseairosa.com>
2015-06-13 20:25:30 +0100

    Trying to fix loss of data on redirection

--- app/controllers/api/v1/load_balancer_controller.rb
+++ app/controllers/api/v1/load_balancer_controller.rb
@@ -11,7 +11,9 @@ class Api::V1::LoadBalancerController < Api::V1::ApiController
       user = User.where(username: params[:name]).first
     end
     response = if user
-                 redirection = "rtmp://#{STREAMERS_CONFIG[user.streamer_id]['private-ip']}/#{params[:app]}/#{params[:name]}?stream_key=#{params[:stream_key]}"
+                 redirection = "rtmp://#{STREAMERS_CONFIG[user.streamer_id]['private-ip']}/#{params[:app]}/#{params[:name]}"
+                 redirection += "?#{params.to_query}"
+                 # redirection += "?stream_key=#{params[:stream_key]}" if params[:stream_key]
                  Rails.logger.info "Redirecting stream #{params[:name]}/#{params[:app]} to #{redirection}"
                  {json: {response: 'ok'}, status: 302, location: redirection}
                else
