50c6c91bfe3446daa6ad3dff7358c7d23b66a434
José P. Airosa <me@joseairosa.com>
2015-06-13 20:11:58 +0100

    Fixing load balancer for non streams

--- app/controllers/api/v1/load_balancer_controller.rb
+++ app/controllers/api/v1/load_balancer_controller.rb
@@ -3,9 +3,15 @@ class Api::V1::LoadBalancerController < Api::V1::ApiController
   include ChannelHelper
 
   def stream
-    user = User.where(username: params[:name], stream_key: params[:stream_key]).first
+    if params[:app] == 'stream'
+      user = User.where(username: params[:name], stream_key: params[:stream_key]).first
+    else
+      user = User.where(username: params[:name]).first
+    end
     response = if user
-                 {json: {response: 'ok'}, status: 302, location: "rtmp://#{STREAMERS_CONFIG[user.streamer_id]['private-ip']}/#{params[:app]}/#{params[:name]}?stream_key=#{params[:stream_key]}"}
+                 redirection = "rtmp://#{STREAMERS_CONFIG[user.streamer_id]['private-ip']}/#{params[:app]}/#{params[:name]}?stream_key=#{params[:stream_key]}"
+                 Rails.logger.info "Redirecting stream #{params[:name]}/#{params[:app]} to #{redirection}"
+                 {json: {response: 'ok'}, status: 302, location: redirection}
                else
                  {json: {response:'user_streamer_id_not_found'}, status: 404}
                end
