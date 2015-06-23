a9899ffe2f734bb7f935455bde34c5d9062a96a1
José P. Airosa <me@joseairosa.com>
2015-06-23 18:54:59 +0100

    Updating customer on stripe

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,7 +5,11 @@ class PaymentsController < ApplicationController
     private_session = PrivateSession.find(params['payment']['object'])
     if private_session && PrivateSessionService.instance.is_not_participant?(private_session, current_user)
 
-      unless current_user.stripe_customer_id
+      if current_user.stripe_customer_id
+        customer = Stripe::Customer.retrieve(current_user.stripe_customer_id)
+        customer.source = params['payment']['id']
+        customer.save
+      else
         customer = Stripe::Customer.create(
             :email => current_user.email,
             :card  => params['payment']['id']
@@ -13,8 +17,7 @@ class PaymentsController < ApplicationController
         current_user.update(stripe_customer_id: customer.id)
       end
       charge = Stripe::Charge.create(
-          :customer    => current_user.stripe_customer_id,
-          :source      => params['payment']['id'],
+          :customer    => customer.id,
           :amount      => (private_session.price*100).to_i,
           :description => "Signup to #{private_session.title}",
           :currency    => 'usd'
