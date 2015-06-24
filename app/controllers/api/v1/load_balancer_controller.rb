124fe04f75f8bf3a2c6044ef41137a487d5dad36
José P. Airosa <me@joseairosa.com>
2015-06-24 21:04:01 +0100

    Load balancer should also support private sessions

--- app/controllers/api/v1/load_balancer_controller.rb
+++ app/controllers/api/v1/load_balancer_controller.rb
@@ -3,20 +3,31 @@ class Api::V1::LoadBalancerController < Api::V1::ApiController
   include ChannelHelper
 
   def stream
-    if params[:app] == 'stream'
-      user = User.where(username: params[:name], stream_key: params[:stream_key]).first
-    else
-      user = User.where(username: params[:name].split('@').first).first
-    end
+    @stream_name, @quality = params[:name].split('@')
+
+    user = if @stream_name.start_with?('PS-')
+             if params[:app] == 'stream'
+               PrivateSession.where(token: @stream_name, stream_key: params[:stream_key]).try(:user)
+             else
+               PrivateSession.where(token: @stream_name).try(:user)
+             end
+           else
+             if params[:app] == 'stream'
+               User.where(username: @stream_name, stream_key: params[:stream_key]).first
+             else
+               User.where(username: @stream_name).first
+             end
+           end
+
     response = if user
-                 redirection = "rtmp://#{STREAMERS_CONFIG[user.streamer_id]['private-ip']}/#{params[:app]}/#{params[:name]}"
+                 redirection = "rtmp://#{STREAMERS_CONFIG[user.streamer_id]['private-ip']}/#{params[:app]}/#{@stream_name}"
                  # redirection += "?#{params.to_query}"
                  redirection += "?stream_key=#{params[:stream_key]}" if params[:stream_key]
-                 Rails.logger.info "Redirecting stream #{params[:name]}/#{params[:app]} to #{redirection}"
-                 StatisticService.instance.load_balancer(302, ip: STREAMERS_CONFIG[user.streamer_id]['private-ip'], app: params[:app], name: params[:name])
+                 Rails.logger.info "Redirecting stream #{@stream_name}/#{params[:app]} to #{redirection}"
+                 StatisticService.instance.load_balancer(302, ip: STREAMERS_CONFIG[user.streamer_id]['private-ip'], app: params[:app], name: @stream_name)
                  {json: {response: 'ok'}, status: 302, location: redirection}
                else
-                 StatisticService.instance.load_balancer(404, app: params[:app], name: params[:name])
+                 StatisticService.instance.load_balancer(404, app: params[:app], name: @stream_name)
                  {json: {response:'user_streamer_id_not_found'}, status: 404}
                end
 
