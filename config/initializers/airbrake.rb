825f78e365ccbbb9384f13119117c3d9fde683a5
José P. Airosa <me@joseairosa.com>
2015-06-24 19:09:39 +0100

    Testing some more

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,5 @@
 Rails.logger.info ENV['AIRBRAKE_CODEWATCH_KEY']
+Rails.logger.info ENV['STRIPE_PUBLISHABLE_KEY']
 Airbrake.configure do |config|
   config.ignore_only  = []
   config.api_key = ENV['AIRBRAKE_CODEWATCH_KEY']
