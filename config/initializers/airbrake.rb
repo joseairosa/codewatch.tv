b366fead581f39e692f22d2d89eb8c46b60c3032
José P. Airosa <me@joseairosa.com>
2015-06-24 19:32:46 +0100

    Testing ENV

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,4 @@
-Rails.logger.info ENV['AIRBRAKE_CODEWATCH_KEY']
-Rails.logger.info ENV['STRIPE_PUBLISHABLE_KEY']
+p ENV
 Airbrake.configure do |config|
   config.ignore_only  = []
   config.api_key = ENV['AIRBRAKE_CODEWATCH_KEY']
