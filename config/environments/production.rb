3e40216f4e86dbb726fee02cc59f0b9b0921eb3f
José P. Airosa <me@joseairosa.com>
2015-06-25 17:15:39 +0100

    Fixed URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,6 @@ Codewatch::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.asset_host = 'https://codewatch.tv'
+  config.default_url = 'http://codewatch.tv'
+  config.action_mailer.asset_host = 'http://codewatch.tv'
 end
