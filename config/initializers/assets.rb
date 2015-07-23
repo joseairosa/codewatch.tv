c7f1b34291bae09df01a5f216f06d636acab9bc8
José P. Airosa <me@joseairosa.com>
2015-07-23 18:40:39 +0100

    Implemented tinymce changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,5 @@
 Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << 'app/assets/stylesheets/tinymce/skins'
+Rails.application.config.assets.paths << 'app/assets/javascripts/tinymce/skins'
 Rails.application.config.assets.precompile += %w( jwplayer/jwplayer.flash.swf sky-forms-ie8.css respond.js placeholder-IE-fixes.js )
 Rails.application.config.assets.precompile << 'emoji/**/*.png'
